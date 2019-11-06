afbc2e26c72df8c160113bae4ca6f2cee42f82be
Alice <achang@health2sync.com>
2019-11-06 17:00:59 +0800

    How to Detect Touch on Real World 3D Objects

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         sceneView.delegate = self
         
@@ -55,6 +57,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             // Create a session configuration
             let configuration = ARWorldTrackingConfiguration()
             
+            configuration.planeDetection = .horizontal
+            
             // Run the view's session
             sceneView.session.run(configuration)
             
@@ -72,5 +76,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+}
+
+extension ViewController :ARSCNViewDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if !results.isEmpty {
+                print("touch plant")
+            } else {
+                print("somewhere else")
+            }
+        }
+    }
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMeterial = SCNMaterial()
+            gridMeterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [gridMeterial]
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+        } else {
+            return
+        }
     }
 }
