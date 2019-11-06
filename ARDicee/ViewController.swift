bd70be03061895e57202466a7176cc0b90c62b1a
Alice <achang@health2sync.com>
2019-11-06 17:38:54 +0800

    How to Animate 3D Objects in AR

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -95,6 +95,16 @@ extension ViewController :ARSCNViewDelegate {
                                                    hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                                                    hitResult.worldTransform.columns.3.z)
                     sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    let randomX = Float(arc4random()%4 + 1) * (Float.pi/2)
+                    let randomZ = Float(arc4random()%4 + 1) * (Float.pi/2)
+                    
+                    diceNode.runAction(
+                        SCNAction.rotateBy(x: CGFloat(randomX * 5),
+                                           y: 0,
+                                           z: CGFloat(randomZ * 5),
+                                           duration: 0.5))
+                    
                 }
             }
         }
